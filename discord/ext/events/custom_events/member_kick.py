97b15e7c2a4ae4923487a4d74cfffcc4eaa1bffc
James <gobot1234yt@gmail.com>
2020-10-18 22:57:29 +0100

    Re-rewrite the listen_for decorator

--- discord/ext/events/custom_events/member_kick.py
+++ discord/ext/events/custom_events/member_kick.py
@@ -1,20 +1,17 @@
 import discord
 
-from ..utils import fetch_recent_audit_log_entry, listens_for
+from ..utils import fetch_recent_audit_log_entry, listens_for, SENTINEL
 
-EVENT_NAME = 'member_kick'
 
 @listens_for('member_remove')
 async def check_member_kick(client: discord.Client, member: discord.Member):
     guild = member.guild
 
     if not guild.me.guild_permissions.view_audit_log:
-        return
+        return SENTINEL
 
     entry = await fetch_recent_audit_log_entry(client, member.guild, target=member, action=discord.AuditLogAction.kick, retry=3)
     if entry is None:
-        return
+        return SENTINEL
 
-    client.dispatch(EVENT_NAME, member, entry)
-
-EVENT_CALLABLE = check_member_kick
+    return member, entry
